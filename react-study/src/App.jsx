b028ec9406d3b15db89213b411348bdee53e8f73
dev-daeun <devdaeun@gmail.com>
2025-07-28 13:19:39 +0900

    Header 컴포넌트 추가

--- react-study/src/App.jsx
+++ react-study/src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
 
-function App() {
 
+function Header() {
+  return (
+    <header>
+      <h1> This is Header</h1>
+    </header>
+  )
+}
+
+
+// Header 컴포넌트는 App 컴포넌트의 자식 컴포넌트
+function App() {
   return (
     <>
+      <Header/>
       <h1>Hello, React!</h1>
     </>
   )
--- react-study/src/main.jsx
+++ react-study/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+// App: 루트 컴포넌트
 createRoot(document.getElementById('root')).render(
     <App />
 )
