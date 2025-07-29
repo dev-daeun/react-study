b19ee73d5c9f75189bead367fe08aec04c2ae08d
dev-daeun <devdaeun@gmail.com>
2025-07-29 14:43:44 +0900

    useRef() 학습

--- react-study/src/components/RegisterUser.jsx
+++ react-study/src/components/RegisterUser.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 
 
@@ -16,16 +16,28 @@ const Register = () => {
             [e.target.name]: e.target.value,
         });
     }
+
+    // useRef(): DOM 에 직접 접근하는 객체 리턴. state 에 의한 리랜더링에 영향 받지 않음.
+    const inputNameRef = useRef();
+    const inputBirthRef = useRef();
+    const onSubmit = () => {
+        if (input.name == "") {
+            inputNameRef.current.focus();
+        }
+        else if (input.birth == "") { 
+            inputBirthRef.current.focus();
+        }
+    };
     return (
         <>
             <h3>회원가입</h3>
             <h4>이름</h4>
             <div>
-                <input name="name" placeholder="이름" onChange={onChange} value={input.name}/>
+                <input name="name" ref={inputNameRef} placeholder="이름" onChange={onChange} value={input.name}/>
             </div>
             <h4>생년월일</h4>
             <div>
-                <input name="birth" type="date" onChange={onChange} value={input.birth}/>
+                <input name="birth" ref={inputBirthRef} type="date" onChange={onChange} value={input.birth}/>
             </div>
             <h4>국적</h4>
             <div>
@@ -39,6 +51,7 @@ const Register = () => {
             <div>
                 <textarea name="bio" onChange={onChange} value={input.bio}/>
             </div>
+            <button onClick={onSubmit}>제출</button>
         </>
     )
 }
